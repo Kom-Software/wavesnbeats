31d5ffe9955e423f16a0be81215463032542e3eb
timpo14 <168415187+timpo14@users.noreply.github.com>
2024-09-12 16:48:39 +0300

    👽️Hide secret key

--- site/wavesnbeats/settings.py
+++ site/wavesnbeats/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yxjx6l4#-_2rfv$to4@qyh2%hb+qi95bl((pc=+4ntqzwyh_g2'
+SECRET_KEY = 'HIDDEN'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
